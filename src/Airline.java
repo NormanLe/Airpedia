c21e21db1e5bc227d80572584a54ddc68eeb9034
normanle <norman.le@stonybrook.edu>
2017-11-16 21:17:23 -0500

    classes so far

---
+++ src/Airline.java
@@ -0,0 +1,16 @@
+
+public class Airline {
+	private String name;
+	
+	public Airline(){
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
