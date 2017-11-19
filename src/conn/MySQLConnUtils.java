a40736ab61ea4f95782c2010d68fd37df6ef9327
Ruby Dong <ruby.dong@stonybrook.edu>
2017-11-19 15:14:19 -0500

    Added more parameters and stopped warnings for mysql connection

--- src/conn/MySQLConnUtils.java
+++ src/conn/MySQLConnUtils.java
@@ -18,10 +18,10 @@ public class MySQLConnUtils {
 	public static Connection getMySQLConnection(String hostName, String dbName, String userName, String password)
 			throws SQLException, ClassNotFoundException {
 
-		Class.forName("com.mysql.jdbc.Driver");
+		Class.forName("com.mysql.cj.jdbc.Driver");
 
 		String connectionURL = "jdbc:mysql://" + hostName + ":3306/" + dbName;
-
+		connectionURL += "?useLegacyDatetimeCode=false&serverTimezone=EST&autoReconnect=true&useSSL=false";
 		Connection conn = DriverManager.getConnection(connectionURL, userName, password);
 		return conn;
 	}
